d49a4ad4d9530a0f7a747dfa63d4e2d2c7fcbf70
Ubuntu <ubuntu@ip-172-31-36-105.us-west-2.compute.internal>
2013-07-10 20:24:00 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    var helloOutput = fs.readFile('index.html');
+    var fs = require('fs');
+    var buf = new Buffer(fs.readFileSync('file.html'), 'utf-8');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
